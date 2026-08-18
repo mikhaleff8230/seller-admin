47fb65519f4d050e6595455e1280f9568a678a13
mikhaleff8230 <sale@sancan.ru>
2026-08-18 20:59:01 +0300

    Fix seller login redirect loop

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,10 +2,7 @@ import LoginForm from '@/components/auth/login-form';
 import { useTranslation } from 'next-i18next';
 import type { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { getAuthCredentials, isAuthenticated } from '@/utils/auth-utils';
-import { useRouter } from 'next/router';
 import AuthPageLayout from '@/components/layouts/auth-layout';
-import { Routes } from '@/config/routes';
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
@@ -14,11 +11,6 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => ({
 });
 
 export default function LoginPage() {
-  const router = useRouter();
-  const { token, permissions } = getAuthCredentials();
-  if (isAuthenticated({ token, permissions })) {
-    router.replace(Routes.dashboard);
-  }
   const { t } = useTranslation('common');
 
   return (
